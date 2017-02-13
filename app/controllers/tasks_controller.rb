8c06edeaa01df1a2ee5c12b91ccca8f5516e9186
MarciaOrr <orrmar@swcp.com>
2017-02-13 20:35:40 +0000

    new task form submission jQuery $.post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -18,7 +18,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:done, :title)
+    params.require(:task).permit(:title, :done)
   end
 
 end
